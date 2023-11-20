1162cb8d141448025a0246d64497ff61422dea44
Saras Windecker <saras.windecker@gmail.com>
2023-11-20 16:34:59 +1000

    simplify out the compute function

--- R/create_infection_timeseries.R
+++ R/create_infection_timeseries.R
@@ -1,5 +1,16 @@
 #' Create infection timeseries
 #'
+#' @description WIP function to compute the # of infections per day, given a
+#' log-scaled initial # of infections and a time-varying random effect
+#' controlling the trend of infections over time. The random effect is defined
+#' as a Gaussian Process (GP), and can be applied to the infection # through
+#' three different formulations. (see vignette for details here or something).
+#' Note that this random effect over time is not explicitly specified as an
+#' epidemiologically defined quantity like the growth rate or the reproduction
+#' number. Rather, these quantities are to be calculated from posterior samples.
+#' The function uses an uninformative prior for the initial # of infections ---
+#' currently there are no plans to make this an input param.
+#'
 #' @param n_days_infection length of full date sequence of infection timeseries
 #' @param n_jurisdictions number of jurisdictions, defaults to 1
 #' @param effect_type options include c('infections', 'growth_rate',
@@ -9,7 +20,9 @@
 #' @export
 create_infection_timeseries <- function (n_days_infection,
                                          n_jurisdictions = 1,
-                                         effect_type = 'growth_rate') {
+                                         effect_type = c('infections',
+                                                         'growth_rate',
+                                                         'growth_rate_derivative')) {
 
   # kernel hyperparams
   gp_lengthscale <- greta::lognormal(0, 3) #inverse_gamma(187/9,1157/18)
@@ -25,10 +38,17 @@ create_infection_timeseries <- function (n_days_infection,
   )
 
   # compute infections from gp
-  infection_timeseries <- compute_infections(
-    log_effect = gp,
-    effect_type = effect_type
-  )
+  # prior for initial num. of infections on log scale
+  inits <- lognormal(0, 1, dim = ncol(gp))
+
+  f <- function (inits, gp, type) {
+    switch (type,
+            infections = infections(inits, gp),
+            growth_rate = growth_rate(inits, gp),
+            growth_rate_derivative = growth_rate_derivative(inits, gp))
+  }
+
+  infection_timeseries <- f(inits, gp, effect_type)
 
   greta_arrays <- module(
     gp,
@@ -41,55 +61,36 @@ create_infection_timeseries <- function (n_days_infection,
   return(greta_arrays)
 }
 
-#' WIP function to compute the # of infections per day, given a log-scaled initial # of infections
-#' and a time-varying random effect controlling the trend of infections over time. The random effect
-#' is defined as a Gaussian Process (GP), and can be applied to the infection # through three
-#' different formulations. (see vignette for details here or something). Note that this random
-#' effect over time is not explicitly specified as an epidemiologically defined quantity like the
-#' growth rate or the reproduction number. Rather, these quantities are to be calculated from
-#' posterior samples. The function uses an uninformative prior for the initial # of infections ---
-#' currently there are no plans to make this an input param.
+#' Effect type "infections" formula for infection timeseries
 #'
-#' @param log_effect Gaussian Process object from greta.gp::gp()
-#' @param effect_type options include c('infections', 'growth_rate',
-#'        'growth_rate_derivative')
+#' @param inits prior for initial num. of infections on log scale
+#' @param z gaussian process
 #'
-#' @return
-#' @export
-compute_infections <- function (log_effect,
-                                effect_type = c('infections',
-                                                'growth_rate',
-                                                'growth_rate_derivative')) {
+#' @return infection timeseries
+infections <- function (inits, z) {
+  exp(sweep(z, 2, inits, FUN = '+'))
+}
 
-  effect_type <- match.arg(effect_type)
+#' Effect type "growth_rate" formula for infection timeseries
+#'
+#' @param inits prior for initial num. of infections on log scale
+#' @param z gaussian process
+#'
+#' @return infection timeseries
+growth_rate <- function (inits, z) {
+  log_rt <- z
+  exp(sweep(greta::apply(log_rt, 2, 'cumsum'), 2, inits, FUN = '+'))
+}
 
-  #prior for initial # of infections on log scale
-  inits <- lognormal(0, 1, dim = ncol(log_effect))
-
-  #specify the formula for infections
-  if (effect_type == 'infections') {
-    f <- function(inits, z) exp(sweep(z, 2, inits, FUN = '+'))
-    N <- f(inits, log_effect)
-  }
-
-  if (effect_type == 'growth_rate') {
-    f <- function(inits, z) {
-      log_rt <- z
-      exp(sweep(greta::apply(log_rt, 2, 'cumsum'), 2, inits, FUN = '+'))
-    }
-    N <- f(inits, log_effect)
-  }
-
-  if (effect_type == 'growth_rate_derivative') {
-    f <- function(inits, z) {
-      log_rt_diff <- z
-      log_rt <- greta::apply(log_rt_diff, 2, 'cumsum')
-      exp(sweep(greta::apply(log_rt, 2, 'cumsum'), 2, inits, FUN = '+'))
-    }
-  }
-
-  N <- f(inits, log_effect)
-
-  return(N)
+#' Effect type "growth_rate_derivative" formula for infection timeseries
+#'
+#' @param inits prior for initial num. of infections on log scale
+#' @param z gaussian process
+#'
+#' @return infection timeseries
+growth_rate_derivative <- function (inits, z) {
+  log_rt_diff <- z
+  log_rt <- greta::apply(log_rt_diff, 2, 'cumsum')
+  exp(sweep(greta::apply(log_rt, 2, 'cumsum'), 2, inits, FUN = '+'))
 }
 
