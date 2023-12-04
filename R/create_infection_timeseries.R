878c5c72cfa6d3e6cbe5d51ebb7e9416f5cfd70d
Saras Windecker <saras.windecker@gmail.com>
2023-12-04 16:14:32 +1100

    tidy description

--- R/create_infection_timeseries.R
+++ R/create_infection_timeseries.R
@@ -1,15 +1,15 @@
 #' Create infection timeseries
 #'
-#' @description WIP function to compute the # of infections per day, given a
-#' log-scaled initial # of infections and a time-varying random effect
+#' @description Compute the number of infections per day, given a
+#' log-scaled initial number of infections and a time-varying random effect
 #' controlling the trend of infections over time. The random effect is defined
-#' as a Gaussian Process (GP), and can be applied to the infection # through
-#' three different formulations. (see vignette for details here or something).
-#' Note that this random effect over time is not explicitly specified as an
+#' as a Gaussian Process (GP), and can be applied to the infection number
+#' through three different formulations.
+#'
+#' This random effect over time is not explicitly specified as an
 #' epidemiologically defined quantity like the growth rate or the reproduction
-#' number. Rather, these quantities are to be calculated from posterior samples.
-#' The function uses an uninformative prior for the initial # of infections ---
-#' currently there are no plans to make this an input param.
+#' number. Rather, these quantities are calculated from posterior samples.
+#' The function uses an uninformative prior for the initial number of infections.
 #'
 #' @param n_days_infection length of full date sequence of infection timeseries
 #' @param n_jurisdictions number of jurisdictions, defaults to 1
@@ -55,7 +55,7 @@ create_infection_timeseries <- function (n_days_infection,
 
   greta_arrays <- module(
     gp,
-    infection_timeseries, # greta_array but with date/state attributes
+    infection_timeseries,
     gp_lengthscale,
     gp_variance,
     gp_kernel
