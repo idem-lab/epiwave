7f35170828ed9ff254f5a86730d3426f3aaa3099
Saras Windecker <saras.windecker@gmail.com>
2023-12-04 16:15:05 +1100

    tidy description

--- R/make_incubation_period_cdf.R
+++ R/make_incubation_period_cdf.R
@@ -1,10 +1,10 @@
 #' Make incubation period cumulative density function
 #'
-#' @description Note that in using incubation period for modelling we should
-#' not treat it as time-varying (due to changes in the dominant variant).
-#' Maybe through vignette or some other documentation we should make it clear
-#' that this is not a time-varying biological quantity and the change in
-#' dominant variant should be modelled separately)
+#' @description Define cumulative density function of incubation period.
+#' Parameters for different COVID-19 strains estimated from
+#' https://www.eurosurveillance.org/content/10.2807/1560-7917.ES.2022.27.6.2200042.
+#' This is not a time-varying biological quantity - any changes in the
+#' dominant variant should be modelled separately.
 #'
 #' @param strain
 #'
@@ -13,19 +13,22 @@
 #' @return cumulative density function for incubation period
 #' @export
 make_incubation_period_cdf <- function (strain = c('Omicron',
-                                                   'WT',
-                                                   'Alpha',
-                                                   'Beta/Gamma',
+                                                   # 'WT',
+                                                   # 'Alpha',
+                                                   # 'Beta/Gamma',
                                                    'Delta')) {
 
   strain <- match.arg(strain)
 
   if (strain == 'Omicron') {
 
-    # parameters estimated from
-    # https://www.eurosurveillance.org/content/10.2807/1560-7917.ES.2022.27.6.2200042
+
     days <- 0:28
     cum_density <- stats::pweibull(days, shape = 1.5, scale = 3.6)
+
+  }
+
+  if (strain == 'Delta') {
 
   }
 
