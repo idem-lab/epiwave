edda504c8fbacd6ddb81f2c3598f99d6a9611f2d
Saras Windecker <saras.windecker@gmail.com>
2023-12-04 16:19:31 +1100

    remove deprecated code

--- R/create_observation_model.R
+++ R/create_observation_model.R
@@ -53,38 +53,6 @@ create_observation_model <- function (infection_timeseries,
         prop_mat[, x]
     }))
 
-  # here index expected cases to data that is brought in.
-  # # get indices for subset of infection days that end up in observed/forecast data
-  # observable_infection_idx <- which(full_infection_dates %in% observable_infection_dates)
-  #
-  # # subset infection timeseries to these indices
-  # # note this is the number of infection days that get observed in the observed
-  # # data, it is not the same length as the actual number of observed dates in
-  # # notification series, which is below
-  # infection_observable <- infections_timeseries[observable_infection_idx,]
-  #
-  # # observed days in the obs data itself
-  # # basically this backs out extra left and right days
-  # obs_data_idx <- which(observable_infection_dates %in% as.Date(rownames(count_data)))
-  # # build convolution matrix
-  # n_days_infection_observable <- length(observable_infection_idx)
-
-
-
-  # use validity matrix - default is all valid but can use this to nullify
-  # expected mean cases for dates when reporting is stopped
-  # extend the valid mat to include forecast days by repeating the last row
-  # this ensures for states where data collection has stopped, the forecast is also 0
-
-  # if (!is.matrix(valid_mat)) {
-  #
-  #     valid_mat <- count_data
-  #     valid_mat[] <- TRUE
-  #     valid_idx <- which(as.logical(valid_mat))
-  # } else {
-  #     valid_idx <- which(valid_mat,arr.ind = FALSE)
-  # }
-
   data_idx <- infection_days %in%
     rownames(case_mat)
   expected_cases_idx <- expected_cases[data_idx, ]
@@ -112,7 +80,6 @@ create_observation_model <- function (infection_timeseries,
   case_mat_array <- greta::as_data(
     as.numeric(case_mat)[valid_idx])
 
-  #
   greta::distribution(case_mat_array) <- greta::negative_binomial(
     size[valid_idx],
     prob[valid_idx])
