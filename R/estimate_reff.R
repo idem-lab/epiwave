d913eaeb659e7fcd898539298b0ca1da1ea15806
Saras Windecker <saras.windecker@gmail.com>
2023-12-04 16:14:46 +1100

    tidy formatting and add greta %*% dependency

--- R/estimate_reff.R
+++ R/estimate_reff.R
@@ -1,22 +1,28 @@
 #' Estimate R_effective
 #'
+#' @description A short description...
+#'
+#'
 #' @param infection_timeseries
 #' @param generation_interval_mass_fxns
 #'
-#' @return greta array for reff
+#' @importFrom greta %*%
+#'
+#' @return greta arrays for infectiousness and $R_eff$
 #' @export
 estimate_reff <- function (infection_timeseries,
                            generation_interval_mass_fxns) {
 
-    convolution_matrix <- get_convolution_matrix(
-        generation_interval_mass_fxns, nrow(infection_timeseries))
-    infectiousness <- convolution_matrix %*% infection_timeseries
+  convolution_matrix <- get_convolution_matrix(
+      generation_interval_mass_fxns, nrow(infection_timeseries))
+  infectiousness <- convolution_matrix %*% infection_timeseries
 
-    reff <- infection_timeseries / (infectiousness + .Machine$double.eps)
+  reff <- infection_timeseries / (infectiousness + .Machine$double.eps)
 
-    greta_arrays <- module(
-        infectiousness,
-        reff)
+  greta_arrays <- module(
+    infectiousness,
+    reff)
 
-    return(greta_arrays)
+  return(greta_arrays)
+
 }
