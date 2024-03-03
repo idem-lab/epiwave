423277c08fc8964bc9b7f23e150cd270a0cd4500
Saras Windecker <saras.windecker@gmail.com>
2024-03-03 14:06:30 +1000

    integrate new functions adn classes to small test script

--- run-pmf.R
+++ run-pmf.R
@@ -1,8 +1,9 @@
-source('R/pmf_dev.R')
+# source('R/pmf_dev.R')
 library(dplyr)
+library(lowerGPreff)
 
 dist <- distributional::dist_weibull(shape = 2, scale = 3)
-delay_probs_from_dist <- params_as_delay_dist(dist)
+delay_probs_from_dist <- parametric_dist_to_distribution(dist)
 
 
 
@@ -11,7 +12,8 @@ data <- data.frame(sym_date = rep(seq(from = as.Date('2023-01-2'),
                                       by = 'day'),
                                   each = 20))
 data$notif_date <- data$sym_date + rpois(nrow(data), 4)
-delay_probs_from_data <- data_as_delay_dist(data)
+delay_probs_from_data <- data_to_distribution(data)
 
 
-combined <- combine_massfuns(delay_probs_from_dist, delay_probs_from_data)
+combined <- combine(delay_probs_from_dist,
+                    delay_probs_from_data)
