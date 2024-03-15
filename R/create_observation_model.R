c73db7b68f5ee724f76cd3a472943c22477c5bd3
AugustHao <tianxiao.hao@telethonkids.org.au>
2024-03-15 05:48:49 +0000

    WIP modifications to make observation model data function accommodate wastewater and sero data

--- R/create_observation_model.R
+++ R/create_observation_model.R
@@ -1,49 +1,100 @@
 #' Create observation model
 #'
 #' @description This function constructs the observation model and defines
-#'  likelihood over the observational data (e.g. cases, hospital
-#'  admissions...). It first begets a timeseries of expected observations from
-#'  a timeseries of new infections, the infection-to-observation delay
-#'  distribution(s), and the proportion of infections to be observed.
-#'  Specifically, the model uses the delay distribution(s) to convolve the
-#'  infection timeseries into the expected observation timeseries, thus
-#'  accounting for the probability of observation over time-since-infection. An
-#'  additional multiplication by observation proportion is applied to the
-#'  convolved timeseries, to adjust for effects such as case ascertainment.
-#'  Optionally, a day-of-week effect may be included in the convolution process
-#'  to introduce weekly periodicity. The expected observation timeseries is
-#'  treated as the mean of a negative binomial distribution from which the data
-#'  is observed, thus allowing us to define likelihood over the data, and link
-#'  the data to the unknown infection timeseries.
+#'   likelihood over the observational data (e.g. cases, hospital
+#'   admissions...). It first begets a timeseries of expected observations from
+#'   a timeseries of new infections, the infection-to-observation delay
+#'   distribution(s), and the proportion of infections to be observed.
+#'   Specifically, the model uses the delay distribution(s) to convolve the
+#'   infection timeseries into the expected observation timeseries, thus
+#'   accounting for the probability of observation over time-since-infection. An
+#'   additional multiplication by observation proportion is applied to the
+#'   convolved timeseries, to adjust for effects such as case ascertainment.
+#'   Optionally, a day-of-week effect may be included in the convolution process
+#'   to introduce weekly periodicity. The expected observation timeseries is
+#'   treated as the mean of a negative binomial distribution from which the data
+#'   is observed, thus allowing us to define likelihood over the data, and link
+#'   the data to the unknown infection timeseries.
 #'
 #' @param infection_timeseries greta array of infection timeseries
-#' @param delay_distribution distribution of delays, covering length of
-#'  infection timeseries, including incubation period, if applicable
 #' @param proportion_observed long form data, for all dates of infection
-#'  timeseries, of expected proportions of infections observed in count data
-#' @param count_data long form data of counts of notifications
+#'   timeseries, of expected proportions of infections observed in count data
 #' @param dow_model optional module of greta arrays defining day-of-week model
 #' @param data_id optional name label identifying data type for greta arrays
+#' @param convolution_distribution distribution for convolution, be it delay
+#'   distribution for neg binom count data or decay curve for sero/wastewater
+#'   data
+#' @param obs_data observational data, of any type, in long data format indexed
+#'   by date and jurisdiction
+#' @param binom_sample_sizes total sample size in binomial survey data, in long
+#'   data format
+#' @param total_pops total jurisdiction pop, for calculating infection
+#'   prevalence for seropositivity sample data, need to be a vector matching to
+#'   jurisdiction names in the infection matrix
+#' @param likelihood choice for likelihood over data
+#' @param noise optional cauchy noise
+#' @param lab_y_offset time-constant scaling effect for wastewater data
 #'
 #' @importFrom greta %*% as_data negative_binomial normal sweep zeros
 #'
 #' @return greta arrays of observation model
 #' @export
 create_observation_model <- function (infection_timeseries,
-                                      delay_distribution,
-                                      proportion_observed,
-                                      count_data,
+                                      convolution_distribution,
+                                      proportion_observed = NULL,
+                                      obs_data,
+                                      binom_sample_sizes = NULL,
+                                      total_pops = NULL,
                                       dow_model = NULL,
+                                      likelihood = c('negbinom',
+                                                     'binom',
+                                                     'logNormal'),
+                                      noise = FALSE,
+                                      lab_y_offset = NULL,
                                       data_id = NULL) {
 
   # add if statements to check that infection_days is long enough to cover
   # the right period
 
-  case_mat <- data_to_matrix(count_data, 'count')
-  delay_mat <- data_to_matrix(delay_distribution, 'delay_fxn')
-  prop_mat <- data_to_matrix(proportion_observed, 'prop')
+  # need to avoid match.arg? can change later
+  likelihood <- match.arg(likelihood)
 
-  infection_days <- as.Date(rownames(prop_mat))
+  if (likelihood == 'negbinom') {
+    message("negative binomial likelihood over data!")
+  }
+
+  if (likelihood == 'binom') {
+    message("binomial likelihood over data!")
+  }
+
+  if (likelihood == 'logNormal') {
+    message("log-Normal likelihood over data!")
+  }
+
+  infection_days <- as.Date(rownames(infection_timeseries))
+  jurisdictions <- colnames(infection_timeseries)
+  n_jurisdictions <- ncol(infection_timeseries)
+  n_dates <- nrow(infection_timeseries)
+
+  # construct null prop for wastewater and sero data
+  if (is.null(proportion_observed)) {
+    proportion_observed <- GPreff::expand_constant_value(
+      dates = infection_days,
+      jurisdictions = jurisdictions,
+      constant_val = 1,
+      col_name = 'prop')
+  }
+
+  # for wastewater data, need to account for site level effect, so need to
+  # modify the data_to_matrix function to accommodate the extra column
+
+  if (likelihood == 'logNormal') {
+    obs_mat <- data_with_site_to_matrix(obs_data, 'concentration') # dummy fun
+  } else {
+    obs_mat <- data_to_matrix(obs_data, 'count')
+  }
+  dist_mat <- data_to_matrix(convolution_distribution, 'delay_fxn')
+  prop_mat <- data_to_matrix(proportion_observed, 'prop')
 
   # for hospitalisation, multiply the car by the chr to create ihr
   # this is temp code for covid live demo
@@ -57,68 +108,119 @@ create_observation_model <- function (infection_timeseries,
     prop_mat <- prop_mat * dow_correction
   }
 
-  n_jurisdictions <- ncol(infection_timeseries)
-  n_dates <- nrow(infection_timeseries)
 
   convolution_matrices <- lapply(
     1:n_jurisdictions,
     function(x) {
-      get_convolution_matrix(delay_mat[, x],
+      get_convolution_matrix(dist_mat[, x],
                              n_dates)
     })
 
   # compute expected cases of the same length
-  expected_cases_list <- lapply(
+  expected_obs_list <- lapply(
     1:n_jurisdictions,
     function(x) {
       convolution_matrices[[x]] %*% infection_timeseries[, x] * prop_mat[, x]
     })
 
-  expected_cases <- do.call(
+  expected_obs <- do.call(
     cbind,
-    expected_cases_list
+    expected_obs_list
   )
 
-  data_idx <- infection_days %in% rownames(case_mat)
-  expected_cases_idx <- expected_cases[data_idx, ]
+  if (noise == TRUE) {
+    # Cauchy noise in obs model
+    # add cauchy noise
+    noise_sigma <- 0.001
+    noise_raw <- cauchy(0, 1, dim = c(nrow(expected_obs),
+                                      ncol(expected_obs))
+    )
+    noise <- noise_raw * noise_sigma
+    expected_obs <- expected_obs * exp(noise)
+  }
 
-  n_days <- nrow(case_mat)
+  if (likelihood == 'binom') {
+    # divide by total pop of jurisdictions to get pop positivity rates
+    expected_obs <- sweep(expected_obs,
+                          2,
+                          total_pops,
+                          FUN = "/")
+  }
 
-  # negative binomial parameters
-  sqrt_inv_size <- greta::normal(0, 0.5,
-                                 truncation = c(0, Inf),
-                                 dim = n_jurisdictions)
-  sqrt_inv_size <- greta::sweep(greta::zeros(n_days,
-                                             n_jurisdictions),
-                                2, sqrt_inv_size,
-                                FUN = "+")
 
-  size <- 1 / sqrt(sqrt_inv_size)
-  prob <- 1 / (1 + expected_cases_idx / size)
+  data_idx <- infection_days %in% rownames(obs_mat)
+  expected_obs_idxed <- expected_obs[data_idx, ]
 
-  valid_mat <- case_mat
-  valid_mat[is.na(case_mat)] <- FALSE
-  valid_mat[!is.na(case_mat)] <- TRUE
+  # tally the number of days in the observed data
+  n_days_idxed <- nrow(obs_mat)
+
+  # construct validity matrix
+  valid_mat <- obs_mat
+  valid_mat[is.na(obs_mat)] <- FALSE
+  valid_mat[!is.na(obs_mat)] <- TRUE
   valid_idx <- as.logical(as.numeric(valid_mat))
+  # use it to get the valid array of data
+  obs_mat_array <- greta::as_data(
+    as.numeric(obs_mat)[valid_idx])
 
-  case_mat_array <- greta::as_data(
-    as.numeric(case_mat)[valid_idx])
+  if (likelihood == 'negbinom') {
+    # negative binomial parameters
+    sqrt_inv_size <- greta::normal(0, 0.5,
+                                   truncation = c(0, Inf),
+                                   dim = n_jurisdictions)
+    sqrt_inv_size <- greta::sweep(greta::zeros(n_dates,
+                                               n_jurisdictions),
+                                  2, sqrt_inv_size,
+                                  FUN = "+")
 
-  greta::distribution(case_mat_array) <- greta::negative_binomial(
-    size[valid_idx],
-    prob[valid_idx])
+    size <- 1 / sqrt(sqrt_inv_size)
+    prob <- 1 / (1 + expected_obs / size)
 
-  greta_arrays <- list(
-    size,
-    prob,
-    convolution_matrices
-  )
+    # truncate size and prob to observed date range
+    size_idxed <- size[data_idx, ]
+    prob_idxed <- prob[data_idx, ]
 
-  names(greta_arrays) <- c(
-    paste0(data_id, '_size'),
-    paste0(data_id, '_prob'),
-    paste0(data_id, '_convolution_matrices')
-  )
+    greta::distribution(obs_mat_array) <- greta::negative_binomial(
+      size_idxed[valid_idx],
+      prob_idxed[valid_idx])
+
+    greta_arrays <- list(
+      size,
+      prob,
+      convolution_matrices
+    )
+
+    names(greta_arrays) <- c(
+      paste0(data_id, '_negbinom_size'),
+      paste0(data_id, '_negbinom_prob'),
+      paste0(data_id, '_convolution_matrices')
+    )
+  }
+
+  if (likelihood == 'binom') {
+
+    sample_sizes_mat <- data_to_matrix(binom_sample_sizes, 'total')
+
+    greta::distribution(obs_mat_array) <- greta::binomial(
+      size = sample_sizes_mat[valid_idx],
+      prob = expected_obs_idxed[valid_idx]
+    )
+
+    greta_arrays <- list(
+      expected_obs, # suspect we want to output the full length version?
+      convolution_matrices
+    )
+
+    names(greta_arrays) <- c(
+      paste0(data_id, '_binom_prob'),
+      paste0(data_id, '_convolution_matrices')
+    )
+  }
+
+
+  if (likelihood == 'logNormal') {
+
+  }
 
   return(greta_arrays)
 }
