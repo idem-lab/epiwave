70c2d6688f01134d0cf81a1dfb0748d3ce126d03
Saras Windecker <smwindecker@users.noreply.github.com>
2024-07-02 17:03:42 +1000

    calculate reff outside model definition
    closes #22

---
+++ R/compute_reff.R
@@ -0,0 +1,15 @@
+compute_reff <- function (wavylistything, gi) {
+
+  ## Reff
+  reff_model_objects <- estimate_reff(
+    infection_timeseries = wavylistything$infection_model_objects$infection_timeseries,
+    generation_interval_mass_fxns = gi)
+
+  reff_out <- epiwave.pipelines::generate_long_estimates(
+    reff_model_objects$reff,
+    wavylistything$fit,
+    wavylistything$infection_days,
+    wavylistything$jurisdictions)
+
+  reff_out
+}
