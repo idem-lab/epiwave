2619b3cf0600b44646d498d03fcefe705db54c8a
Saras Windecker <smwindecker@users.noreply.github.com>
2024-10-10 16:30:43 +1000

    move workflow for testing into tests folder so that it doesn't trigger build warnings

--- tests/test_workflow/testing.R
+++ tests/test_workflow/testing.R
@@ -4,64 +4,73 @@ library(epiwave.params)
 library(dplyr)
 library(greta)
 
-study_seq <- seq(from = as.Date('2021-06-01'),
-                 to = as.Date('2021-12-31'), 'days')
 
-## user specific folder with not synced data
+## user modified
+study_seq <- seq(from = as.Date('2021-02-01'),
+                 to = as.Date('2021-12-15'), 'days')
+infection_days <- seq(from = as.Date('2021-01-03'),
+                      to = as.Date('2022-01-12'), 'days')
+jurisdictions <- 'NSW'
+
+# specific folder with not synced data
 not_synced_folder <- '../../BDSS_governance/BDSS/data'
 
-## notification counts
-notif_file <- paste0(not_synced_folder, '/COVID_live_cases.rds')
 
+## data prep
+# notification counts
+notif_file <- paste0(not_synced_folder, '/COVID_live_cases.rds')
 notif_dat <- notif_file |>
   readRDS() |>
   dplyr::rename(value = new_cases) |>
   dplyr::filter(date %in% study_seq)
-
-# notif_dat <- notif_dat[notif_dat$jurisdiction == 'NSW',]
+if (!is.na(jurisdictions)) {
+  notif_dat <- notif_dat[notif_dat$jurisdiction == jurisdictions,]
+}
 class(notif_dat) <- c('epiwave_fixed_timeseries',
                       'epiwave_timeseries',
                       class(notif_dat))
 
-## hospitalisation counts
+# hospitalisation counts
 hosp_file <- paste0(not_synced_folder, '/COVID_live_cases_in_hospital.rds')
-
-hosp_occupancy <- hosp_file |>
+hosp_dat <- hosp_file |>
   readRDS() |>
-  dplyr::filter(date %in% study_seq)
-hosp_dat <- hosp_occupancy |>
+  dplyr::filter(date %in% study_seq) |>
   dplyr::mutate(value = cases_in_hospital/3)
+if (!is.na(jurisdictions)) {
+  hosp_dat <- hosp_dat[hosp_dat$jurisdiction == jurisdictions,]
+}
 class(hosp_dat) <- c('epiwave_fixed_timeseries',
                      'epiwave_timeseries',
                      class(hosp_dat))
 
 # jurisdictions
-jurisdictions <- unique(notif_dat$jurisdiction)
+if (is.na(jurisdictions)) {
+  jurisdictions <- unique(notif_dat$jurisdiction)
+}
 n_jurisdictions <- length(jurisdictions)
 
-## days of infection timeseries
-infection_days <- seq(from = as.Date('2021-05-03'),
-                      to = as.Date('2022-01-29'), 'days')
-
+# days of infection timeseries
 n_days_infection <- length(infection_days)
 
-## proportions
+# proportions
 car_constant <- 0.75
-car <- create_epiwave_fixed_timeseries(
+car <- create_epiwave_timeseries(
   dates = infection_days,
   jurisdictions = jurisdictions,
   value = car_constant) # 1 for sero prop
 
 # create IHR
-ihr <- greta::uniform(0, 1)
-ihr_timeseries <- create_epiwave_fixed_timeseries(
+chr <- greta::uniform(0, 1)
+# ihr <- chr * car
+# wrapper for ihr specific flow
+ihr <- create_epiwave_greta_timeseries(
   dates = infection_days,
   jurisdictions = jurisdictions,
-  value = list(car_constant * ihr)
-)
+  car = car,
+  chr_prior = chr)
 
-### new
 
+## delays
 incubation <- readRDS('tests/test_distributions/incubation.rds')
 gi <- readRDS('tests/test_distributions/gi.rds')
 onset_to_notification <- readRDS('tests/test_distributions/onset_to_notification.rds')
@@ -74,7 +83,7 @@ hosp_full_delay_dist <- create_epiwave_massfun_timeseries(
   value = hosp_delay_ecdf)
 
 
-wavylistything <- fit_waves(
+fit_object <- fit_waves(
   observations = define_observation_model(
     cases = prepare_observation_data(
       timeseries_data = notif_dat,
@@ -87,39 +96,38 @@ wavylistything <- fit_waves(
     hospitalisations = prepare_observation_data(
       timeseries_data = hosp_dat,
       delay = hosp_full_delay_dist,
-      proportion_observed = car,
-      type = "count",
-      ihr_correction = ihr)
+      proportion_observed = ihr,
+      type = "count")
   ),
 
-  # proportion = ,
-  target_infection_dates = infection_days,
-  n_chains = 3,
-  max_convergence_tries = 1,
-  warmup = 100,
-  n_samples = 100,
-  n_extra_samples = 100
+  infection_model = 'gp_growth_rate', #'flat_prior',
+  target_infection_dates = infection_days #,
+  # n_chains = 2,
+  # max_convergence_tries = 2,
+  # warmup = 100,
+  # n_samples = 100,
+  # n_extra_samples = 100
 )
 
-fitted_reff <- compute_reff(wavylistything, gi)
+fitted_reff <- compute_reff(fit_object, gi)
 
 
-# outptus
-infections_out <- GPreff::generate_long_estimates(
-  wavylistything$infection_model$infection_timeseries,
-  wavylistything$fit,
+# outputs
+infections_out <- epiwave.pipelines::generate_long_estimates(
+  fit_object$infection_model$infection_timeseries,
+  fit_object$fit,
   infection_days,
   jurisdictions)
 
-infection_traj <- GPreff::build_trajectories(
-  param = wavylistything$infection_model$infection_timeseries,
+infection_traj <- epiwave.pipelines::build_trajectories(
+  param = fit_object$infection_model$infection_timeseries,
   infection_days,
-  wavylistything$fit,
+  fit_object$fit,
   nsim = 1000,
   jurisdictions)
 
-GPreff::plot_reff_interval_curves(
-  'reff2.png',
+epiwave.pipelines::plot_reff_interval_curves(
+  'gp_both_reff_9Oct.pdf',
   fitted_reff,
   dates = infection_days,
   start_date = min(study_seq),
@@ -127,18 +135,19 @@ GPreff::plot_reff_interval_curves(
   jurisdictions = jurisdictions)
 
 infection_sims <- greta::calculate(
-  wavylistything$infection_model$infection_timeseries,
-  values = wavylistything$fit,
+  fit_object$infection_model$infection_timeseries,
+  values = fit_object$fit,
   nsim = 1000)
 
-GPreff::plot_timeseries_sims(
-  'infection_timeseries2.png',
+epiwave.pipelines::plot_timeseries_sims(
+  'output/gp_both_infections_Kate-9Oct.png',
   infection_sims[[1]],
   type = "infection",
   dates = infection_days,
+  states = jurisdictions,
   start_date = study_seq[1],
   end_date = study_seq[length(study_seq)],
-  states = jurisdictions,
   dim_sim = "2",
-  infection_nowcast = FALSE)
+  infection_nowcast = TRUE,
+  nowcast_start = as.Date('2021-12-08'))
 
