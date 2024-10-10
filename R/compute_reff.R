13c84cfd4befa399db3a2f9a2ee30f8d08dda62c
Saras Windecker <smwindecker@users.noreply.github.com>
2024-10-10 16:26:32 +1000

    consolidate the two reff functions into same file

--- R/compute_reff.R
+++ R/compute_reff.R
@@ -1,15 +1,67 @@
-compute_reff <- function (wavylistything, gi) {
+#' Compute R effective from model outputs and generation interval
+#'
+#' @param fit_output model fit
+#' @param gi generation interval distribution
+#'
+#' @return Estimated R effective
+#' @export
+#'
+compute_reff <- function (fit_output, gi) {
 
-  ## Reff
   reff_model_objects <- estimate_reff(
-    infection_timeseries = wavylistything$infection_model$infection_timeseries,
+    infection_timeseries = fit_output$infection_model$infection_timeseries,
     generation_interval_mass_fxns = gi)
 
   reff_out <- epiwave.pipelines::generate_long_estimates(
     reff_model_objects$reff,
-    wavylistything$fit,
-    wavylistything$infection_days,
-    wavylistything$jurisdictions)
+    fit_output$fit,
+    fit_output$infection_days,
+    fit_output$jurisdictions)
 
   reff_out
+}
+
+#' Estimate R_effective
+#'
+#' @description Calculate a timeseries of effective reproduction number, given a
+#'  timeseries of infections, and the generation interval distribution. This
+#'  function first convolves infections into infectiousness using the
+#'  generation interval distribution. This weights each infectious individual
+#'  by their relative infectivity profile for each day post infection. Then,
+#'  infection timeseries is divided by the infectiousness timeseries to
+#'  calculate instantaneous reproduction number. This interpretation of the
+#'  instantaneous reproduction number is well established and consistent with
+#'  classical discrete renewal models. Note that here the generation interval
+#'  distribution acts as a proxy for infectivity profile distribution since the
+#'  latter cannot be learned from typical epidemiological data. This is
+#'  justified because across population, the distribution of the timing of
+#'  secondary infections relative to the timing of primary infection (i.e. the
+#'  generation interval) should correlate with when the primary individual is
+#'  most infectious. Also note that because time of infection is rarely
+#'  documented and generation interval is therefore hard to measure, in
+#'  practice the serial interval is often used as a further proxy.
+#'
+#' @param infection_timeseries greta array of infection timeseries
+#' @param generation_interval_mass_fxns mass functions describing the
+#'  generation interval
+#'
+#' @importFrom greta %*%
+#'
+#' @return greta arrays for infectiousness and $R_eff$
+#' @export
+estimate_reff <- function (infection_timeseries,
+                           generation_interval_mass_fxns) {
+
+  convolution_matrix <- get_convolution_matrix(
+    generation_interval_mass_fxns,
+    n = nrow(infection_timeseries))
+  infectiousness <- convolution_matrix %*% infection_timeseries
+
+  reff <- infection_timeseries / (infectiousness + .Machine$double.eps)
+
+  greta_arrays <- module(
+    infectiousness,
+    reff)
+
+  return(greta_arrays)
 }
